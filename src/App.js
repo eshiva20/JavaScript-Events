763f33d7ecf72bde16f525fd91c5d032d5a30b62
shiva <shiva@nimapinfotech.com>
2023-02-03 11:31:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <span className="title">JavaScript Events</span>
+      <span className="counter">Count is <span>{count}</span></span>
+      <div className="type" >
+      <div className="mouse-events">
+        <h2>Mouse Events</h2>
+        <button onClick={() => setCount(count + 1)}>onClick</button>
+        <button onMouseOver={() => setCount(count + 1)}>onMouseOver</button>
+        <button onMouseOut={() => setCount(count + 1)}>onMouseOut</button>
+        <button onMouseUp={() => setCount(count + 1)}>onMouseUp</button>
+        <button onMouseDown={() => setCount(count + 1)}>onMouseDown</button>
+        <button onMouseMove={() => setCount(count + 1)}>onMouseMove</button>
+      </div>
+      <div className="keyboard-events">
+        <h2>KeyBoard Events</h2>
+        <input
+          input="text"
+          placeholder="onKeyUp Event"
+          onKeyUp={() => setCount(count + 1)}
+        />
+        <input
+          input="text"
+          placeholder="onKeyDown Event"
+          onKeyDown={() => setCount(count + 1)}
+        />
+        <input
+          input="text"
+          placeholder="onFocus Event"
+          onFocus={() => setCount(count + 1)}
+        />
+        <input
+          input="text"
+          placeholder="onBlur Event"
+          onBlur={() => setCount(count + 1)}
+        />
+        <input
+          input="text"
+          placeholder="onChange Event"
+          onChange={() => setCount(count + 1)}
+        />
+      </div>
+      </div>
     </div>
   );
 }
